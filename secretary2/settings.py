29179f0605d4fd30dffd5031848a616427ae14ac
lmase57130 <lmase57130@gmail.com>
2017-02-09 02:11:56 -0500

    123

--- secretary2/settings.py
+++ secretary2/settings.py
@@ -76,14 +76,16 @@ WSGI_APPLICATION = 'secretary2.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'localhost',
-        'PORT': '',
+       # 'default': {
+       
+       
+       # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       #'HOST': 'localhost',
+       #'PORT': '',
   
-#'default': {
-#    'ENGINE': 'django.db.backends.sqlite3',
-#    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+  'default': {
+  'ENGINE': 'django.db.backends.sqlite3',
+   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
     }
 }
@@ -126,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+]
